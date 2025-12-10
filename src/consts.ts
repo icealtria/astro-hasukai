1c799a19e3668e4387e2d2ce62ccc3410ca39dda
Rin <icealtria@gmail.com>
2025-12-10 22:24:36 +0800

    update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 export const SITE = {
-    url: 'https://kururin.cc',
-    domain: 'kururin.cc',
+    url: 'https://hasukai.vercel.app',
+    domain: 'hasukai.vercel.app',
     title: "Hasukai",
     description: 'A astro theme for blog.',
     slogan: 'Eat, Sleep & Daydream',
